8d9f3619967b6dbbf1b5fbedcb8d0e5862b2a21d
Edvard Halmrast Lentz <edvardhlentz@gmail.com>
2022-03-11 14:25:19 +0100

    add function that converts image to selected format and scales for nn

---
+++ image_formatter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import pandas as pd
+import numpy as np
+import cv2
+
+df = pd.read_csv("color_ranges.csv")
+
+color_ranges = {}
+for _, row in df.iterrows():
+    if row["format"] not in color_ranges:
+        color_ranges[row["format"]] = []
+    color_ranges[row["format"]].append((row["min"],row["range"]))
+
+def convert_image(image, format):
+    if format == "HLS":
+        image = np.asarray(cv2.cvtColor(image, cv2.COLOR_BGR2HLS_FULL))
+    elif format == "HSV":
+        image = np.asarray(cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL))
+    elif format == "Lab":
+        image = np.asarray(cv2.cvtColor(image, cv2.COLOR_BGR2Lab))
+    elif format == "Luv":
+        image = np.asarray(cv2.cvtColor(image, cv2.COLOR_BGR2Luv))
+    elif format == "RGB":
+        image = np.asarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    elif format == "XYZ":
+        image = np.asarray(cv2.cvtColor(image, cv2.COLOR_BGR2XYZ))
+    elif format == "YCrCb":
+        image = np.asarray(cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb))
+    elif format == "YUV":
+        image = np.asarray(cv2.cvtColor(image, cv2.COLOR_BGR2YUV))
+    image = image.astype(float)
+    for channel in range(image.shape[2]):
+        image[:,:,channel] = (image[:,:,channel] - color_ranges[format][channel][0])/color_ranges[format][channel][1]
+    return image
+
