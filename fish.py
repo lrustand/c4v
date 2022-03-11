c3e53743f248ddf00ae4a6eafc795bcdac61cda1
Edvard Halmrast Lentz <edvardhlentz@gmail.com>
2022-03-11 11:05:59 +0100

    change fish.py to use scaling from color_ranges.csv

--- fish.py
+++ fish.py
@@ -2,26 +2,40 @@
 ### Taken from tensorflow documentation
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 
 from tensorflow.keras import datasets, layers, models
 import cv2, glob
 from sklearn.model_selection import train_test_split
 
+color_space = "BGR"
+
+df = pd.read_csv("datasets/fish/final_all_index.txt", sep="=", header=None)
+df_cr = pd.read_csv("color_ranges.csv")
+
+df_cr = df_cr.loc[df_cr.iloc[:,0]==color_space,"min":].reset_index()
+
+files = df.iloc[:,3]
+all_labels = df.iloc[:,0]
+img_types = df.iloc[:,2]
+
 images = []
 labels = []
-with open("datasets/fish/final_all_index.txt") as f:
-    for line in f.readlines():
-        file = line.split("=")[3]
-        label = line.split("=")[1]
-        label_nr = int(line.split("=")[0])
-        img_type = line.split("=")[2]
-        if img_type == "insitu":
-            img = cv2.imread("datasets/fish/images/cropped/" + file + ".png")
 
-            img = cv2.resize(img, (32,32))
-            cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-            images.append(np.asarray(img) / 255.0)
-            labels.append(label_nr)
+for row in range(len(files)):
+    if img_types[row] == "insitu":
+        img = cv2.imread("datasets/fish/images/cropped/" + files[row] + ".png")
+
+        img = cv2.resize(img, (32,32))
+        if color_space != "BGR":
+            img = cv2.cvtColor(img, cv2.__dict__["COLOR_BGR2" + color_space])
+        img = np.asarray(img).astype(float)
+        for channel in range(img.shape[2]):
+            img[:,:,channel] = img[:,:,channel] - df_cr.iloc[channel,0]
+            img[:,:,channel] = img[:,:,channel] / df_cr.iloc[channel,2]
+
+        images.append(img)
+        labels.append(all_labels[row])
 
 images = np.array(images)
 labels = np.array(labels)
