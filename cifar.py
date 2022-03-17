f18c21f54696be47d313681e9ef52dd571d5544d
Edvard Halmrast Lentz <edvardhlentz@gmail.com>
2022-03-17 13:04:53 +0100

    changed cifar to have same load function as other datasets

--- cifar.py
+++ cifar.py
@@ -1,47 +1,51 @@
 #!/usr/bin/env python
 ### Taken from tensorflow documentation
 import tensorflow as tf
-
+import image_formatter
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
 
 import cv2
 import callback
 
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+def load(width=32, height=32):
+    (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+    for image in train_images:
+        cv2.resize(image, (width, height))
+    for image in test_images:
+        cv2.resize(image, (width, height))
 
+    return train_images, test_images, train_labels, test_labels
 #for images in (train_images, test_images):
 #    for i in images:
 #        cv2.cvtColor(i, cv2.COLOR_BGR2BAYER_RG)
 
 # Normalize pixel values to be between 0 and 1
-train_images, test_images = train_images / 255.0, test_images / 255.0
 
+if __name__ == "__main__":
+    color_space = "BGR"
+    train_images, test_images, train_labels, test_labels = load()
+    train_images = image_formatter.convert_images(train_images, color_space)
+    test_images = image_formatter.convert_images(test_images, color_space)
 
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
+    model.add(layers.Flatten())
+    model.add(layers.Dense(32, activation='relu'))
+    model.add(layers.Dense(max(train_labels.max(), test_labels.max())+1))
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.summary()
 
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
+    asd = callback.test()
 
-model.summary()
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
 
-asd = callback.test()
-
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-history = model.fit(train_images, train_labels, epochs=10,
-                    validation_data=(test_images, test_labels), callbacks=[asd])
-
-
+    history = model.fit(train_images, train_labels, epochs=100,
+                        validation_data=(test_images, test_labels), callbacks=[asd])
