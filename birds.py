19bdf0e7c42c776a6d0dc78c14a1325b4bee6199
Lars Rustand <rustand.lars@gmail.com>
2022-03-17 10:10:41 +0100

    Fix birds

--- birds.py
+++ birds.py
@@ -15,28 +15,25 @@ test_labels = []
 with open("datasets/birds/class_dict.csv") as f:
     for line in f.readlines():
         bird = line.split(",")[1]
-        for file in glob.glob('datasets/birds/train' + bird + '/*.jpg'):
+        for file in glob.glob('datasets/birds/train/' + bird + '/*.jpg'):
             img = cv2.imread(file)
 
             img = cv2.resize(img, (32,32))
             #cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
             train_images.append(np.asarray(img) / 255.0)
             train_labels.append(int(line.split(",")[0]))
-        for file in glob.glob('datasets/birds/test' + bird + '/*.jpg'):
+        for file in glob.glob('datasets/birds/test/' + bird + '/*.jpg'):
             img = cv2.imread(file)
 
             img = cv2.resize(img, (32,32))
             #cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
             test_images.append(np.asarray(img) / 255.0)
             test_labels.append(int(line.split(",")[0]))
-        label += 1
-        label += 1
 
-train_images = np.array(images)
-train_labels = np.array(labels)
-test_images = np.array(images)
-test_labels = np.array(labels)
-
+train_images = np.array(train_images)
+train_labels = np.array(train_labels)
+test_images = np.array(test_images)
+test_labels = np.array(test_labels)
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
