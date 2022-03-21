2fd8be79594f562a2baeae6cf1e6d24602f023d4
Edvard Halmrast Lentz <edvardhlentz@gmail.com>
2022-03-21 14:31:23 +0100

    save csv and graphs in main

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import pandas as pd
 import math
 
 import image_formatter
-import callback
 import model
 import aircrafts
 import birds
@@ -14,20 +13,19 @@ import cifar
 import fish
 import plants
 
-cycle = []
-angle = 0
-for _ in image_formatter.color_spaces:
-    cycle.append(col.hsv_to_rgb([angle, 1, 1]))
-    phi = (1 + math.sqrt(5))/2
-    angle += 1/phi
-    angle %= 1
-angle = 0
-for _ in image_formatter.color_spaces:
-    cycle.append(col.hsv_to_rgb([angle, 0.5, 1]))
-    phi = (1 + math.sqrt(5))/2
-    angle += 1/phi
-    angle %= 1
-matplotlib.cycler(color=cycle)
+#cycle = []
+#angle = 0
+#for _ in image_formatter.color_spaces:
+#    cycle.append(col.hsv_to_rgb([angle, 1, 1]))
+#    phi = (1 + math.sqrt(5))/2
+#    angle += 1/phi
+#    angle %= 1
+#angle = 0
+#for _ in image_formatter.color_spaces:
+#    cycle.append(col.hsv_to_rgb([angle, 0.5, 1]))
+#    phi = (1 + math.sqrt(5))/2
+#    angle += 1/phi
+#    angle %= 1
 
 
 def main():
@@ -42,29 +40,39 @@ def main():
         (orig_train_images, orig_test_images,
          train_labels, test_labels) = dataset.load()
         out_size = max(train_labels.max(), test_labels.max())+1
-        for color_space in ["RGB", "HSV"]:
+        for color_space in image_formatter.color_spaces:
             train_images = image_formatter.convert_images(orig_train_images,
                                                           color_space)
             test_images = image_formatter.convert_images(orig_test_images,
                                                          color_space)
 
             my_model = model.model(out_size)
-            history = my_model.fit(train_images, train_labels, epochs=10,
+            history = my_model.fit(train_images, train_labels, epochs=100,
                                validation_data=(test_images, test_labels))
             plot_label = ds + "_" + color_space
             loss[plot_label] = history.history["loss"]
-            val_loss["val_" + plot_label] = history.history["val_loss"]
+            val_loss[plot_label] = history.history["val_loss"]
             accuracy[plot_label] = history.history["accuracy"]
-            val_accuracy["val_" + plot_label] = history.history["val_accuracy"]
+            val_accuracy[plot_label] = history.history["val_accuracy"]
+
 
         loss_df = pd.DataFrame(loss)
         val_loss_df = pd.DataFrame(val_loss)
         accuracy_df = pd.DataFrame(accuracy)
         val_accuracy_df = pd.DataFrame(val_accuracy)
 
-        full_loss_df = pd.concat([loss_df, val_loss_df], axis=1)
-        full_loss_df.plot(kind="line")
+        loss_df.plot(kind="line")
+        loss_df.to_csv(ds + "_loss.csv")
         plt.savefig(ds + "_loss.png")
+        val_loss_df.plot(kind="line")
+        val_loss_df.to_csv(ds + "_val_loss.csv")
+        plt.savefig(ds + "_val_loss.png")
+        accuracy_df.plot(kind="line")
+        accuracy_df.to_csv(ds + "_accuracy.csv")
+        plt.savefig(ds + "_accuracy.png")
+        val_accuracy_df.plot(kind="line")
+        val_accuracy_df.to_csv(ds + "_val_accuracy.csv")
+        plt.savefig(ds + "_val_accuracy.png")
 
 
 if __name__ == "__main__":
