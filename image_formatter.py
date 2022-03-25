3e3469ad7835945398ff9729cdef8e020614aa6b
Lars Rustand <rustand.lars@gmail.com>
2022-03-25 14:14:14 +0100

    Fix typo

--- image_formatter.py
+++ image_formatter.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import cv2
 
-color_spaces = ["BGR", "HSV", "HSL", "YUV", "YCrCb", "Lab", "Luv", "XYZ"]
+color_spaces = ["BGR", "HSV", "HLS", "YUV", "YCrCb", "Lab", "Luv", "XYZ"]
 
 df = pd.read_csv("color_ranges.csv")
 
