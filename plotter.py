3348fc263e23bd8c13682efde76b5ef5ee9ba757
Lars Rustand <rustand.lars@gmail.com>
2022-03-25 14:39:11 +0100

    Remove debug prints

--- plotter.py
+++ plotter.py
@@ -6,7 +6,5 @@ files = sys.argv[1:]
 
 for f in files:
     df = pd.read_csv(f, index_col=0)
-    print(df.shape)
-    print(df.head())
     df.plot(kind="line")
     plt.savefig(f[:-4] + "_csv.png")
