e4f20aab89c8f9398b48df6ed95ac770d6be210a
Edvard Halmrast Lentz <edvardhlentz@gmail.com>
2022-03-25 10:51:45 +0100

    full loop with 5 runs and 25 epochs

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as col
 import pandas as pd
 import math
+from pathlib import Path
 
 import image_formatter
 import model
@@ -29,50 +30,74 @@ import plants
 
 
 def main():
+    models={"small":model.mini_model, "normal": model.model}
     datasets = {"fish": fish, "birds": birds, "plants": plants,
                 "cifar": cifar, "aircrafts": aircrafts}
-    for ds in datasets:
-        dataset = datasets[ds]
-        loss = {}
-        accuracy = {}
-        val_loss = {}
-        val_accuracy = {}
-        (orig_train_images, orig_test_images,
-         train_labels, test_labels) = dataset.load()
-        out_size = max(train_labels.max(), test_labels.max())+1
-        for color_space in image_formatter.color_spaces:
-            train_images = image_formatter.convert_images(orig_train_images,
-                                                          color_space)
-            test_images = image_formatter.convert_images(orig_test_images,
-                                                         color_space)
+    for current_model in models:
+        for run in range(5):
+            for ds in datasets:
+                dataset = datasets[ds]
+                loss = {}
+                accuracy = {}
+                val_loss = {}
+                val_accuracy = {}
+                (orig_train_images, orig_test_images,
+                 train_labels, test_labels) = dataset.load()
+                out_size = max(train_labels.max(), test_labels.max())+1
+                for color_space in image_formatter.color_spaces:
+                    train_images = image_formatter.convert_images(orig_train_images,
+                                                                  color_space)
+                    test_images = image_formatter.convert_images(orig_test_images,
+                                                                 color_space)
 
-            my_model = model.model(out_size)
-            history = my_model.fit(train_images, train_labels, epochs=100,
-                               validation_data=(test_images, test_labels))
-            plot_label = ds + "_" + color_space
-            loss[plot_label] = history.history["loss"]
-            val_loss[plot_label] = history.history["val_loss"]
-            accuracy[plot_label] = history.history["accuracy"]
-            val_accuracy[plot_label] = history.history["val_accuracy"]
+                    my_model=models[current_model](out_size)
+                    history = my_model.fit(train_images, train_labels, epochs=25,
+                                       validation_data=(test_images, test_labels))
+                    plot_label = ds + "_" + color_space
+                    loss[plot_label] = history.history["loss"]
+                    val_loss[plot_label] = history.history["val_loss"]
+                    accuracy[plot_label] = history.history["accuracy"]
+                    val_accuracy[plot_label] = history.history["val_accuracy"]
 
+                    loss_df = pd.DataFrame(loss)
+                    val_loss_df = pd.DataFrame(val_loss)
+                    accuracy_df = pd.DataFrame(accuracy)
+                    val_accuracy_df = pd.DataFrame(val_accuracy)
 
-        loss_df = pd.DataFrame(loss)
-        val_loss_df = pd.DataFrame(val_loss)
-        accuracy_df = pd.DataFrame(accuracy)
-        val_accuracy_df = pd.DataFrame(val_accuracy)
+                    common_path = f"{run+1}/{current_model}_{ds}."
+                    val_acc_path = f"runs/val/acc/{common_path}"
+                    val_loss_path = f"runs/val/loss/{common_path}"
+                    test_acc_path = f"runs/test/acc/{common_path}"
+                    test_loss_path = f"runs/test/loss/{common_path}"
 
-        loss_df.plot(kind="line")
-        loss_df.to_csv(ds + "_loss.csv")
-        plt.savefig(ds + "_loss.png")
-        val_loss_df.plot(kind="line")
-        val_loss_df.to_csv(ds + "_val_loss.csv")
-        plt.savefig(ds + "_val_loss.png")
-        accuracy_df.plot(kind="line")
-        accuracy_df.to_csv(ds + "_accuracy.csv")
-        plt.savefig(ds + "_accuracy.png")
-        val_accuracy_df.plot(kind="line")
-        val_accuracy_df.to_csv(ds + "_val_accuracy.csv")
-        plt.savefig(ds + "_val_accuracy.png")
+                    filepath = Path(val_acc_path)
+                    filepath.parent.mkdir(parents=True, exist_ok=True)
+                    filepath = Path(val_loss_path)
+                    filepath.parent.mkdir(parents=True, exist_ok=True)
+                    filepath = Path(test_acc_path)
+                    filepath.parent.mkdir(parents=True, exist_ok=True)
+                    filepath = Path(test_loss_path)
+                    filepath.parent.mkdir(parents=True, exist_ok=True)
+
+                    loss_df.plot(kind="line")
+                    plt.savefig(f"{test_loss_path}png")
+                    loss_df.to_csv(f"{test_loss_path}csv")
+                    plt.close("all")
+
+                    val_loss_df.plot(kind="line")
+                    plt.savefig(f"{val_loss_path}png")
+                    val_loss_df.to_csv(f"{val_loss_path}csv")
+                    plt.close("all")
+
+                    accuracy_df.plot(kind="line")
+                    plt.savefig(f"{test_acc_path}png")
+                    accuracy_df.to_csv(f"{test_acc_path}csv")
+                    plt.close("all")
+
+                    val_accuracy_df.plot(kind="line")
+                    plt.savefig(f"{val_acc_path}png")
+                    val_accuracy_df.to_csv(f"{val_acc_path}csv")
+                    plt.close("all")
 
 
 if __name__ == "__main__":
