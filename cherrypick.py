9629faa048811f5bddff8be8550194dc9da0ef99
Edvard Halmrast Lentz <edvardhlentz@gmail.com>
2022-03-29 09:50:45 +0200

    change cherrypick to log number of flops

--- cherrypick.py
+++ cherrypick.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
-
+flops = {}
 for val_test in ["val", "test"]:
     for acc_loss in ["acc", "loss"]:
         for small_normal in ["small", "normal"]:
@@ -13,8 +13,17 @@ for val_test in ["val", "test"]:
                     path = f"runs/{val_test}/{acc_loss}/{run}/{small_normal}_{ds}.csv"
                     try:
                         new_df = pd.read_csv(path, index_col=0)
+                        old_columns = new_df.columns
                         new_df = new_df.loc[:, new_df.var() > 0.00001]
+                        new_columns = new_df.columns
                         df.append(new_df)
+                        for column in old_columns:
+                            if column not in new_columns:
+                                full_column = f"{column},{val_test},{acc_loss},{small_normal}"
+                                if full_column in flops:
+                                    flops[full_column] += 1
+                                else:
+                                    flops[full_column] = 1
                     except:
                         break
                     run += 1
@@ -24,3 +33,5 @@ for val_test in ["val", "test"]:
                     filepath = Path(path)
                     filepath.parent.mkdir(parents=True, exist_ok=True)
                     df.to_csv(path)
+for flop in flops:
+    print(f"{flop},{flops[flop]}")
